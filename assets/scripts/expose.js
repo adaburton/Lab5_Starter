23563841a102ff34cc5a1350e824aa04f0edb839
adaburton <theadaburton@gmail.com>
2022-10-29 11:19:25 -0700

    implement volume slider

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -7,23 +7,23 @@ function init() {
 
   //initialize everything.
   updateSound();
+  updateVolume();
 
   // Alter the image and sound based on the user's selection in horn-select
-  document.getElementById("horn-select").addEventListener('change', function(){
-    console.log("Horn Select: I'm being Changed!");
-    //console.log(document.getElementById("horn-select").value);
-    updateSound();
-  });
+  document.getElementById("horn-select").addEventListener('change', updateSound);
+
+  // Alter the page to reflect the user's selection
+  document.getElementById("volume").addEventListener('click', updateVolume);
   
+  
+
 }
 
 //Updates the page to reflect the user's selection
 function updateSound(){
-  var img = document.querySelector("header + img");
-  var snd = document.querySelector("audio");
+  let img = document.querySelector("header + img");
+  let snd = document.querySelector("audio");
   
-
-
   switch(document.getElementById("horn-select").value){
     case "air-horn":
       // air settings
@@ -54,9 +54,34 @@ function updateSound(){
   console.log(snd.getAttribute("src"));
 }
 
-/*
-updateSoundIcon(){
+// Alter the page to reflect the user's selection
+function updateVolume(){
+  let vol = document.getElementById("volume").value;
+  let img = document.querySelector("[id='volume-controls'] > img");
+  let snd = document.querySelector("audio");
 
+  switch(true){
+    case vol == 0:
+      snd.volume = 0.0;
+      img.setAttribute("src", "assets/icons/volume-level-0.svg");
+      break;
+    case vol < 33:
+      snd.volume = vol / 100.0;
+      img.setAttribute("src", "assets/icons/volume-level-1.svg");
+      break;
+    case vol < 67:
+      snd.volume = vol / 100.0;
+      img.setAttribute("src", "assets/icons/volume-level-2.svg");
+      break;
+    case vol < 100:
+      snd.volume = vol / 100.0;
+      img.setAttribute("src", "assets/icons/volume-level-3.svg");
+      break;
+    default:
 
-}*/
+  }
+  console.log(vol);
+  console.log(img.getAttribute("alt"));
+  console.log(snd.volume);
+}
 
